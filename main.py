7ca51de526b842854c5643db2ed9fc2b15f79bf3
skibidi <a@b.c>
2023-02-15 15:29:35 +0300

    init commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -13,13 +13,33 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def skibidi(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text= '420')
 
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text_caps = ' '.join(context.args).upper()
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token('6255294638:AAEpSF3f_7Y-DUqYGc16vcKexeIFojHEEP8').build()
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+    caps_handler = CommandHandler('caps', caps)
+    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+
 
     start_handler = CommandHandler('start', start)
     skibidi_handler = CommandHandler('skibidi', skibidi)
 
     application.add_handler(skibidi_handler)
     application.add_handler(start_handler)
+    application.add_handler(caps_handler)
+    application.add_handler(unknown_handler)
+    application.add_handler(echo_handler)
 
     application.run_polling()
